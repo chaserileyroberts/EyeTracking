bfef7d9e2e81a0fc7afd20cb069af6090fe70c25
Chase Roberts <keeper6928@gmail.com>
2017-09-26 17:48:44 -0400

    Made hardcoded paths easier to deal with

--- run_training_all_kang_data.py
+++ run_training_all_kang_data.py
@@ -2,28 +2,32 @@ import Trainer
 import tensorflow as tf
 
 # TODO(Chase) Bad hack, run it for now.
+test_data = [
+  'kang/day01/Center/data.mat',
+  'kang/day01/Left/data.mat',
+  'kang/day01/Right/data.mat'
+]
 kang_center_files = [
-  './data_kang/day01/Center/data.mat',
-  './data_kang/day02/Center/data.mat',
-  './data_kang/day03/Center/data.mat',
-  './data_kang/day04/Center/data.mat',
-  './data_kang/day05/Center/data.mat',
-  './data_kang/day06/Center/data.mat'
+  'kang/day02/Center/data.mat',
+  'kang/day03/Center/data.mat',
+  'kang/day04/Center/data.mat',
+  'kang/day05/Center/data.mat',
+  'kang/day06/Center/data.mat'
   ]
 kang_left_files = [
-  './data_kang/day01/Left/data.mat',
-  './data_kang/day02/Left/data.mat',
-  './data_kang/day03/Left/data.mat',
-  './data_kang/day04/Left/data.mat',
-  './data_kang/day05/Left/data.mat',
+
+  'kang/day02/Left/data.mat',
+  'kang/day03/Left/data.mat',
+  'kang/day04/Left/data.mat',
+  'kang/day05/Left/data.mat',
   ]
 kang_right_files = [
-  './data_kang/day01/Right/data.mat',
-  './data_kang/day02/Right/data.mat',
-  './data_kang/day03/Right/data.mat',
-  './data_kang/day04/Right/data.mat',
-  './data_kang/day05/Right/data.mat',
-  './data_kang/day06/Right/data.mat'
+
+  'kang/day02/Right/data.mat',
+  'kang/day03/Right/data.mat',
+  'kang/day04/Right/data.mat',
+  'kang/day05/Right/data.mat',
+  'kang/day06/Right/data.mat'
   ]
 
 all_kang_data = kang_center_files + kang_left_files + kang_right_files
@@ -31,5 +35,7 @@ kang_small = kang_center_files[:2]
 
 if __name__ == '__main__':
   sess = tf.Session()
+  path = "./"
+  kang_small = [path + x for x in kang_small]
   trainer = Trainer.Trainer(sess, kang_small, batch_size=1, save_dest="./models")
   trainer.train(10)
