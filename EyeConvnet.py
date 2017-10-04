52e6a3922fc6d3429e47dfaa1f28746d39686dc2
Chase Roberts <keeper6928@gmail.com>
2017-10-04 13:59:22 -0400

    Wow I am an idiot...

--- EyeConvnet.py
+++ EyeConvnet.py
@@ -76,17 +76,14 @@ class EyeConvnet():
                           normalizer_fn=slim.batch_norm,
                           normalizer_params={'is_training': self.is_training}):
         net = slim.conv2d(image_input, 32, [11, 11], scope="conv1_11x11")
+        net = slim.conv2d(net, 64, [5, 5], scope="conv2_5x5")
         net = slim.max_pool2d(net, [4, 4], stride=4, scope='pool1')
-        net = slim.conv2d(image_input, 64, [5, 5], scope="conv2_5x5")
-        net = slim.max_pool2d(net, [4, 4], stride=4, scope='pool1')
-        net = slim.conv2d(image_input, 64, [5, 5], scope="conv3_5x5")
-        net = slim.max_pool2d(net, [2, 2], scope='pool4')
-        net = slim.conv2d(image_input, 128, [3, 3], scope="conv4_3x3")
+        net = slim.conv2d(net, 64, [5, 5], scope="conv3_5x5")
+        net = slim.conv2d(net, 128, [3, 3], scope="conv4_3x3")
         net = slim.max_pool2d(net, [2, 2], scope='pool2')
-        net = slim.conv2d(image_input, 128, [3, 3], scope="conv5_3x3")
+        net = slim.conv2d(net, 128, [3, 3], scope="conv5_3x3")
         net = slim.max_pool2d(net, [2, 2], scope='pool3')
-        net = slim.conv2d(image_input, 32, [1, 1], scope="conv6_1x1")            
-        net = slim.max_pool2d(net, [2, 2], scope='pool4')
+        net = slim.conv2d(net, 32, [1, 1], scope="conv6_1x1")            
         return net
     
   def make_eye_branch(self, image_input):
