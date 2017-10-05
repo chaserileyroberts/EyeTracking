5e69b815576132941f987f0ae672d00581b99d0a
Chase Roberts <keeper6928@gmail.com>
2017-10-05 15:06:15 -0400

    Added evaluation loop

--- run_training_all_kang_data.py
+++ run_training_all_kang_data.py
@@ -1,7 +1,11 @@
 import Trainer
 import tensorflow as tf
+import argparse
 
-# TODO(Chase) Bad hack, run it for now.
+parser = argparse.ArgumentParser(description='Run Eye Tracking Convnet Training.')
+parser.add_argument( '-evaluate', '-e', action='store_true', default=False)
+args = parser.parse_args()
+
 test_data = [
   'kang/day01/Center/data.mat',
   'kang/day01/Left/data.mat',
@@ -35,5 +39,9 @@ all_kang_data = kang_center_files + kang_left_files + kang_right_files
 if __name__ == '__main__':
   path = "/media/roberc4/kang/final_dataset/"
   data_paths = [path + x for x in all_kang_data]
-  trainer = Trainer.Trainer(data_paths, batch_size=32, save_dest='/media/roberc4/kang/models/')
+  trainer = Trainer.Trainer(
+      data_paths, 
+      batch_size=32, 
+      save_dest='/media/roberc4/kang/models/',
+      eval_loop=args.evaluate)
   trainer.train()
